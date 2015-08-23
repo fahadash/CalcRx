de1fcceb42b21c8371122db90eeff8306f3d9b8a
fahadash <me@fahadash.com>
2015-08-22 23:42:11 -0400

    Fix CalcRx extensions, add function packs

--- CalcRx/Evaluator.cs
+++ CalcRx/Evaluator.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using FormulaParser;
+using FormulaParser.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +13,35 @@ namespace CalcRx
     {
         public static Func<IObservable<TInput>, IObservable<TOutput>> Evaluate<TInput, TOutput>(this IObservable<TInput> source, string expression)
         {
-//##TODO: Finish working on this extension
-            return null;
+            return CreateExpression<TInput, TOutput>(source, expression, null);
         }
+
+        public static Func<IObservable<TInput>, IObservable<TOutput>> Evaluate<TInput, TOutput>(this IObservable<TInput> source, string expression, IEnumerable<Function> functions)
+        {
+            return CreateExpression<TInput, TOutput>(source, expression, functions);
+        }
+
+        private static Func<IObservable<TInput>, IObservable<TOutput>> CreateExpression<TInput, TOutput>(IObservable<TInput> source, string expression, IEnumerable<Function> functions = null)
+        {
+            ExpressionParser parser;
+
+            if (functions == null)
+            {
+                parser = new ExpressionParser();
+            }
+            else
+            {
+                parser = new ExpressionParser(functions.ToList());
+            }
+
+            var baseExpr = Expression.Parameter(source.GetType());
+
+            var exp = parser.BuildExpression(expression, baseExpr);
+
+            var lambda = Expression.Lambda(exp, baseExpr);
+
+            return (Func<IObservable<TInput>, IObservable<TOutput>>)lambda.Compile();
+        }
+    
     }
 }
---
+++ CalcRx/FunctionPacks.cs
@@ -0,0 +1,70 @@
+﻿using FormulaParser.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcRx
+{
+    public class FunctionPacks
+    {
+        public static IEnumerable<Function> BasicAggregationFunctionPack
+        {
+            get
+            {
+                return new[] 
+                {
+                    new Function("SUM", 
+                        new Func<IObservable<double>, IObservable<double>>(
+                        o => o.Scan(0.0d, (acc, item) => item + acc))),
+                    new Function("MIN", 
+                        new Func<IObservable<double>, IObservable<double>>(
+                        o => o.Scan(double.MinValue, (acc, item) => MinOf(acc, item)))),
+                    new Function("MAX", 
+                        new Func<IObservable<double>, IObservable<double>>(
+                        o => o.Scan(double.MinValue, (acc, item) => MaxOf(acc, item)))),
+                    new Function("COUNT", 
+                        new Func<IObservable<double>, IObservable<double>>(
+                        o => o.Scan(0.0d, (acc, _) => acc + 1))),
+                };
+            }            
+        }
+        public static IEnumerable<Function> HistoryFunctionPack
+        {
+            get
+            {
+                return new[] 
+                {
+                    new Function("REF", 
+                        new Func<IObservable<double>, double, IObservable<double>>(
+                            //TODO: Change the Grammar to parse int and double separately
+                            //  so 2nd parameter can be int
+                        (o, count) =>  o.Zip(o.Skip(Convert.ToInt32(count)), (a, b) => a)))
+                };
+            }            
+        }
+
+
+
+        public static double MinOf(double a, double b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+
+            return b;
+        }
+        public static double MaxOf(double a, double b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+
+            return b;
+        }
+    }
+}
